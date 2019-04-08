ffb6f9924119d3a8c8ae970aa790be03743b0118
Eduardo Alonso <eduardo.alonsoh@gmail.com>
2019-04-08 06:07:46 +0000

    corrección

--- date.c
+++ date.c
@@ -6,6 +6,6 @@
 int main(int argc, char **aa){
 	struct rtcdate rtc ;
 	date(&rtc);
-	printf(1, "%d:%d:%d %d-%d-%d\n", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
+	printf(1, "%d-%d-%d %d:%d:%d\n", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
 	exit();
 } 
